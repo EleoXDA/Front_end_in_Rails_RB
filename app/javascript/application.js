de8e38023356db21d820b502328c324fd5820221
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-24 23:28:24 +0200

    IMPORT bootstrap to JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "bootstrap"
+
 
 console.log("Hello from app/javascript/application.js")
