e4c8399b8776f37f928cdb4805f3d9000aa98923
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-24 21:32:47 +0200

    ADD: Routes to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'pages#home'
 end
