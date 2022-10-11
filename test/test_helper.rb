9a1bbb79106e953bf6d62cc5aa895ebb3a7b1946
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-11 23:29:48 +0200

    update test helper file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,22 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+# frozen_string_literal: true
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+# top-level documentation
+module ActiveSupport
+  # top-level documentation
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
-  # Add more helper methods to be used by all tests here...
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
 end
+
+# Folder path for screenshots
+Capybara.save_path = Rails.root.join('tmp/capybara')
